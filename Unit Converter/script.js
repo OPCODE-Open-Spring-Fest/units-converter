9b3d120212c82d03072fa01f57bed9eb572e2215
saurabhm6341 <saurabhmishra6341@gmail.com>
2024-04-04 23:36:31 +0530

    #1 [feat] created a seperate css and javascript file for navbar and linked to each html pages without removing any feature

--- Unit Converter/script.js
+++ Unit Converter/script.js
@@ -1,10 +1,1 @@
 //Javascript for index.html will be written here
-
-function showSidebar(){
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'flex'
-}
-function hideSidebar(){
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'none'
-}
